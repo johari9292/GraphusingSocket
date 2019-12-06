dc4ebd801f4632aa0283a112b9d40474cc5734b5
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2019-12-07 04:02:44 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@
 const mongoose = require("mongoose");
 
 // const connectionstr =  "mongodb://localhost:27017/local";
-// const connectionstr = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
-const MONGO_URI = "mongodb://joharibalti:is119821885@ds227352.mlab.com:27352/heroku_1kkl8s2q"
+const connectionstr = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
+// const MONGO_URI = "mongodb://joharibalti:is119821885@ds227352.mlab.com:27352/heroku_1kkl8s2q"
 
 
 
-mongoose.connect(MONGO_URI,{useCreateIndex: true, useNewUrlParser: true }).then(
+mongoose.connect(connectionstr ,{useCreateIndex: true, useNewUrlParser: true }).then(
   () => {
     console.log("Database connection established!");
   },
