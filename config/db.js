b156232d0fb5bdb110aced66a8694975a0970dea
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2019-12-10 10:28:43 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 
 // const connectionstr =  "mongodb://localhost:27017/local";
-const connectionstr = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
 
 
 
-mongoose.connect(connectionstr,{useCreateIndex: true, useNewUrlParser: true }).then(
+mongoose.connect(MONGO_URI,{useCreateIndex: true, useNewUrlParser: true }).then(
   () => {
     console.log("Database connection established!");
   },
