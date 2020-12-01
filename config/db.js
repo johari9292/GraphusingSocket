507f5b99f138fff7753065e553c2c9e116c6c3cb
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2020-12-01 17:59:55 +0500

    hh

--- config/db.js
+++ config/db.js
@@ -1,17 +1,20 @@
-
 const mongoose = require("mongoose");
 
 // const connectionstr =  "mongodb://localhost:27017/local";
-const MONGO_URI = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
+const MONGO_URI =
+  "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
 
-
-
-mongoose.connect(MONGO_URI,{useCreateIndex: true, useNewUrlParser: true }).then(
-  () => {
-    console.log("Database connection established!");
-  },
-  err => {
-    console.log("Error connecting Database instance due to: ", err);
-  }
-);
-
+mongoose
+  .connect(MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connection established!");
+    },
+    (err) => {
+      console.log("Error connecting Database instance due to: ", err);
+    }
+  );
