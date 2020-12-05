9f68f437226af5fb5dd1350d8defd718ef5ad184
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2020-12-05 20:02:09 +0500

    hgfh

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,8 +8,8 @@ router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
 
 router
   .route("/api/users/:userId")
-  .get(authCtrl.requireSignin, userCtrl.read)
-  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .get(userCtrl.read)
+  .put(userCtrl.update)
   .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
 router.param("userId", userCtrl.userByID);
