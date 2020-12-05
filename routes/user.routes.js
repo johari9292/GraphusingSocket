d66943e302ec4d866e46d06fb08316ab2543a83f
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2020-12-06 00:19:04 +0500

    fff

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 
 router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
 
-router
-  .route("/api/users/:userId")
-  .get(authCtrl.requireSignin, userCtrl.read)
-  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
-  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
+// router
+//   .route("/api/users/:userId")
+//   .get(authCtrl.requireSignin, userCtrl.read)
+//   .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+//   .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
-router.param("userId", userCtrl.userByID);
+// router.param("userId", userCtrl.userByID);
 router.route("/getuser/:id").get(userCtrl.gettodobyid);
 
 // router.route("/updateuser/:id").post(userCtrl.updatetodo);
