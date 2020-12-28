d7bec984a6c0aed61ceb7086581643770619bd19
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2020-12-29 00:18:39 +0500

    dgdfg

--- controllers/balance.controller.js
+++ controllers/balance.controller.js
@@ -12,15 +12,13 @@ exports.gettodo = (req, res) => {
 };
 
 exports.deletetodo = (req, res) => {
-  Todo.remove(
-    { _id: req.params.id }).then((err) => {
-      if (!err) {
-        res.status(200).send({ status: "deleted" });
-      } else {
-        res.status(500).send({ status: "error" });
-      }
-    })
-  );
+  Todo.remove({ _id: req.params.id }, function (err) {
+    if (!err) {
+      res.status(200).send({ status: "deleted" });
+    } else {
+      res.status(500).send({ status: "error" });
+    }
+  });
 };
 
 exports.gettodobyid = (req, res) => {
