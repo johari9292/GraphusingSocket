2343900dcf7b7009494c7aabcd0fb4ce606f13d7
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2021-02-19 04:49:40 +0500

    dvddv

--- routes/balance.routes.js
+++ routes/balance.routes.js
@@ -7,7 +7,18 @@ router.route("/getbalance/:id").get(balanceController.gettodo);
 
 router.route("/getbalanceforadmin/").get(balanceController.gettodobyid);
 
-router.route("/addbalance").post(balanceController.addtodo);
+router
+  .route("/addbalance")
+  .post(balanceController.addtodo)
+  .options(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    return res.send(200);
+  });
 
 router.route("/deletebalance/:id").delete(balanceController.deletetodo);
 router.route("/updatebalance/:id").post(balanceController.updatetodo);
