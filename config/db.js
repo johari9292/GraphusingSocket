b663ef49ec244f498f4492240f40bfb16f87926c
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2021-03-08 03:06:27 +0500

    update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // const connectionstr =  "mongodb://localhost:27017/local";
 const MONGO_URI =
   // "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
-  "mongodb+srv://joharibalti1996:is119821885@gaveagroplataforma.ynzpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://hola:Arlegui140$@gaveagroplataforma.ynzpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose
   .connect(MONGO_URI, {
     useCreateIndex: true,
